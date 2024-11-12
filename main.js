4c5d391143be413d98bbacd4df69a7d375aca355
arekusandorara <mamimumemo9137@yahoo.co.jp>
2024-11-12 15:57:43 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+  const swiper = new Swiper('.swiper', {
+    grabCursor: true,
+    watchSlidesProgress: true,
+    loop: true,
+    loopAdditionalSlides: 1,
+    centeredSlides: true,
+    autoplay: true,
+    effect: 'fade',
+    speed: 3000,
+  });
+}
